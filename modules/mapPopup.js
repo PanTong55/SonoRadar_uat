08e6af4918e62cfb90d93fc764b803264acd8d65
PanTong55 <pantong0505@gmail.com>
2025-11-11 05:44:14 +0000

    fix css 3

--- modules/mapPopup.js
+++ modules/mapPopup.js
@@ -546,7 +546,7 @@ export function initMapPopup({
                     closeButton: false,
                     autoClose: false,
                     closeOnClick: false,
-                    offset: L.point(-6, -14)
+                    offset: L.point(-6, -12)
                   });
                   marker.openPopup();
                   marker._tooltipPinned = true;
